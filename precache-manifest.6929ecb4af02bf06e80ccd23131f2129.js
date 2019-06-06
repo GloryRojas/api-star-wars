00810914cde412c9c7507b27e2b63b8520a8adab
GloryRojas <grojasm94@gmail.com>
2019-06-06 15:51:53 -0500

    Updates

---
+++ precache-manifest.6929ecb4af02bf06e80ccd23131f2129.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c92f131549391493029e183d0bdb33d",
+    "url": "/api-star-wars/index.html"
+  },
+  {
+    "revision": "e551a402b9e123b9215e",
+    "url": "/api-star-wars/static/css/main.5f118b61.chunk.css"
+  },
+  {
+    "revision": "9dad43a26e9a9bf6ef2f",
+    "url": "/api-star-wars/static/js/2.8aab9229.chunk.js"
+  },
+  {
+    "revision": "e551a402b9e123b9215e",
+    "url": "/api-star-wars/static/js/main.97ca564b.chunk.js"
+  },
+  {
+    "revision": "5f782fc881326eaba47b",
+    "url": "/api-star-wars/static/js/runtime~main.8a8ef1e4.js"
+  }
+]);
