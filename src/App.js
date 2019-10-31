f4636762e69a18bafb26fbbd6d496f9d72e5a6f3
GloryRojas <grojasm94@gmail.com>
2019-10-30 23:40:45 -0500

    Add rimac page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Inicio from "./components/Inicio";
 import Personaje from "./components/Personaje";
+import Rimac from "./components/Rimac";
 
 export const App = () => {
   
   return(
     <Router basename={process.env.PUBLIC_URL}>
-      <Route path="/" exact component={Inicio}>
-      </Route>
-      <Route path="/Personaje/" component={Personaje}>
-      </Route>
+      <Route path="/" exact component={Inicio}></Route>
+      <Route path="/Personaje/" component={Personaje}></Route>
+      <Route path="/Rimac/" component={Rimac}></Route>
     </Router>
   )
 };
